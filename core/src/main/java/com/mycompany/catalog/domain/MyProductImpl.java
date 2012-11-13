49c6516f85313c937daf300f1c5c69ac4025fc9f
Jerry Ocanas <jerry77oz@gmail.com>
2012-11-13 16:10:04 -0600

    Extended category and Product now implements lazy initialization for embeddables; set some fields to retrun default values from parent object.

--- core/src/main/java/com/mycompany/catalog/domain/MyProductImpl.java
+++ core/src/main/java/com/mycompany/catalog/domain/MyProductImpl.java
@@ -26,138 +26,156 @@ public class MyProductImpl extends ProductImpl implements Product, SeoMetaData, 
     @Embedded
     protected TwitterDataImpl twitterData = new TwitterDataImpl();
 
-    public SeoMetaDataImpl getSeoMetaData() {
-        return seoMetaData;
+    protected void initializeSeoMetaData(){
+        if(seoMetaData == null){
+            seoMetaData = new SeoMetaDataImpl();
+        }
     }
 
-    public void setSeoMetaData(SeoMetaDataImpl seoMetaData) {
-        this.seoMetaData = seoMetaData;
-    }
-
-    public TwitterDataImpl getTwitterData() {
-        return twitterData;
-    }
-
-    public void setTwitterData(TwitterDataImpl twitterData) {
-        this.twitterData = twitterData;
+    protected void initializeTwitterData(){
+        if(twitterData == null){
+            twitterData = new TwitterDataImpl();
+        }
     }
 
     @Override
     @Nullable
     public String getMetaDescription() {
-        return seoMetaData.getMetaDescription();
+        initializeSeoMetaData();
+        return seoMetaData.getMetaDescription() != null ? seoMetaData.getMetaDescription() : super.getLongDescription();
     }
 
     @Override
     public void setMetaDescription(@Nullable String metaDescription) {
+        initializeSeoMetaData();
         seoMetaData.setMetaDescription(metaDescription);
     }
 
     @Override
     @Nullable
     public String getMetaKeywords() {
+        initializeSeoMetaData();
         return seoMetaData.getMetaKeywords();
     }
 
     @Override
     public void setMetaKeywords(@Nullable String metaKeywords) {
+        initializeSeoMetaData();
         seoMetaData.setMetaKeywords(metaKeywords);
     }
 
     @Override
     @Nullable
     public String getMetaRobot() {
+        initializeSeoMetaData();
         return seoMetaData.getMetaRobot();
     }
 
     @Override
     public void setMetaRobot(@Nullable String metaRobot) {
+        initializeSeoMetaData();
         seoMetaData.setMetaRobot(metaRobot);
     }
 
     @Override
     @Nullable
     public String getTitleFragment() {
+        initializeSeoMetaData();
         return seoMetaData.getTitleFragment();
     }
 
     @Override
     public void setTitleFragment(@Nullable String titleFragment) {
+        initializeSeoMetaData();
         seoMetaData.setTitleFragment(titleFragment);
     }
 
     @Override
     public String getTwitterSite() {
+        initializeTwitterData();
         return twitterData.getTwitterSite();
     }
 
     @Override
     public void setTwitterSite(@Nullable String twitterSite) {
+        initializeTwitterData();
         twitterData.setTwitterSite(twitterSite);
     }
 
     @Override
     public String getTwitterCreator() {
+        initializeTwitterData();
         return twitterData.getTwitterCreator();
     }
 
     @Override
     public void setTwitterCreator(@Nullable String twitterCreator) {
+        initializeTwitterData();
         twitterData.setTwitterCreator(twitterCreator);
     }
 
     @Override
     @Nullable
     public String getTwitterCard() {
+        initializeTwitterData();
         return twitterData.getTwitterCard();
     }
 
     @Override
     public void setTwitterCard(@Nullable String twitterCard) {
+        initializeTwitterData();
         twitterData.setTwitterCard(twitterCard);
     }
 
     @Override
     @Nullable
     public String getTwitterUrl() {
-        return twitterData.getTwitterUrl();
+        initializeTwitterData();
+        return twitterData.getTwitterUrl() != null ? twitterData.getTwitterUrl() : super.getUrl();
     }
 
     @Override
     public void setTwitterUrl(@Nullable String twitterUrl) {
+        initializeTwitterData();
         twitterData.setTwitterUrl(twitterUrl);
     }
 
     @Override
     @Nullable
     public String getTwitterTitle() {
+        initializeTwitterData();
         return twitterData.getTwitterTitle();
     }
 
     @Override
     public void setTwitterTitle(@Nullable String twitterTitle) {
+        initializeTwitterData();
         twitterData.setTwitterTitle(twitterTitle);
     }
 
     @Override
     @Nullable
     public String getTwitterDescription() {
+        initializeTwitterData();
         return twitterData.getTwitterDescription();
     }
 
     @Override
     public void setTwitterDescription(@Nullable String twitterDescription) {
+        initializeTwitterData();
         twitterData.setTwitterDescription(twitterDescription);
     }
 
     @Override
     @Nullable
     public String getTwitterImage() {
+        initializeTwitterData();
         return twitterData.getTwitterImage();
     }
 
     @Override
     public void setTwitterImage(@Nullable String twitterImage) {
+        initializeTwitterData();
         twitterData.setTwitterImage(twitterImage);
     }
 }
