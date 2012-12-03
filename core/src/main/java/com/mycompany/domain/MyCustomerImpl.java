412b429979b1117d9ffc3e1668316ec52acab26b
Priyesh Patel <ppatel@broadleafcommerce.org>
2012-12-03 16:25:51 -0600

    add customer price lists

---
+++ core/src/main/java/com/mycompany/domain/MyCustomerImpl.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2008-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mycompany.domain;
+
+import org.broadleafcommerce.pricelist.domain.PriceList;
+import org.broadleafcommerce.pricelist.domain.customer.PriceListCustomer;
+import org.broadleafcommerce.pricelist.domain.customer.PriceListCustomerImpl;
+import org.broadleafcommerce.pricelist.domain.order.PriceListOrderImpl;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+
+
+
+/**
+ * @author Priyesh Patel
+ */
+@Entity
+public class MyCustomerImpl extends org.broadleafcommerce.profile.core.domain.CustomerImpl implements org.broadleafcommerce.profile.core.domain.Customer,PriceListCustomer {
+    private static final long serialVersionUID = -1849002347743308131L;
+    
+    @Embedded
+    protected PriceListCustomerImpl embeddablePriceList = new PriceListCustomerImpl();
+
+    @Override
+    public PriceList getPriceList() {
+        return embeddablePriceList.getPriceList();
+    }
+
+    @Override
+    public void setPriceList(PriceList priceList) {
+        embeddablePriceList.setPriceList(priceList);
+    }
+    
+}
