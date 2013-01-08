01c4bd8b8c89e2fec117e1c3bc9f2abf1606162f
Priyesh Patel <ppatel@broadleafcommerce.org>
2013-01-07 18:56:07 -0600

    1) removing useDefaultIfNotFound boolean from entity 2) fix admin to show proper prominent fields  in the list

--- core/src/main/resources/sql/load_content_structure_international.sql
+++ core/src/main/resources/sql/load_content_structure_international.sql
@@ -34,9 +34,9 @@ INSERT INTO BLC_LOCALE (LOCALE_CODE, DEFAULT_FLAG, FRIENDLY_NAME, CURRENCY_CODE)
 -- Load price lists:  Your site will need to define 'price list data' populated with the prices. This demo has based
 -- the price lists on currencies, but your site could use areas (zip codes, etc) to define lists.
 --
-INSERT INTO BLC_PRICE_LIST (PRICE_LIST_ID,CURRENCY_CODE,FRIENDLY_NAME,PRICE_KEY,USE_DEFAULT_FLAG) VALUES (2,'EUR','EUR Dollar','EUR',false);
-INSERT INTO BLC_PRICE_LIST (PRICE_LIST_ID,CURRENCY_CODE,FRIENDLY_NAME,PRICE_KEY,USE_DEFAULT_FLAG) VALUES (3,'GBP','GB Pound','GBP',false);
-INSERT INTO BLC_PRICE_LIST (PRICE_LIST_ID,CURRENCY_CODE,FRIENDLY_NAME,PRICE_KEY,USE_DEFAULT_FLAG) VALUES (4,'MXN','Mexican Peso','MXN',false);
+INSERT INTO BLC_PRICE_LIST (PRICE_LIST_ID,CURRENCY_CODE,FRIENDLY_NAME,PRICE_KEY) VALUES (2,'EUR','EUR Dollar','EUR');
+INSERT INTO BLC_PRICE_LIST (PRICE_LIST_ID,CURRENCY_CODE,FRIENDLY_NAME,PRICE_KEY) VALUES (3,'GBP','GB Pound','GBP');
+INSERT INTO BLC_PRICE_LIST (PRICE_LIST_ID,CURRENCY_CODE,FRIENDLY_NAME,PRICE_KEY) VALUES (4,'MXN','Mexican Peso','MXN');
 
 --
 -- The following items create page templates.   The key is to map a JSP template (TMPLT_PATH) to
@@ -51,3 +51,27 @@ INSERT INTO BLC_PAGE_TMPLT (PAGE_TMPLT_ID, LOCALE_CODE, TMPLT_NAME, TMPLT_DESCR,
 INSERT INTO BLC_PGTMPLT_FLDGRP_XREF(PAGE_TMPLT_ID, FLD_GROUP_ID, GROUP_ORDER) VALUES (2,1,0);
 
 
+--if GBP then use EUR
+insert into BLC_PL_CRITERIA_RULE values(1,'MVEL.eval("toUpperCase()",locale.localeCode).contains(MVEL.eval("toUpperCase()","en_GB"))')
+insert into BLC_PRICE_LIST_RULE(PRICE_LIST_RULE_ID,PRICE_LIST,PRIORITY,FRIENDLY_NAME) VALUES(1,3,1,'GB Pound if contains en_GB');
+INSERT INTO BLC_PL_RULE_MAP VALUES (1,1,'LOCALE');
+--if US then use EUR
+insert into BLC_PL_CRITERIA_RULE values(2,'MVEL.eval("toUpperCase()",locale.localeCode).contains(MVEL.eval("toUpperCase()","en_US"))')
+insert into BLC_PRICE_LIST_RULE(PRICE_LIST_RULE_ID,PRICE_LIST,PRIORITY,FRIENDLY_NAME) VALUES(2,1,2,'US Dollar if contains en_US');
+INSERT INTO BLC_PL_RULE_MAP VALUES (2,2,'LOCALE');
+--if GBP then use EUR
+insert into BLC_PL_CRITERIA_RULE values(3,'MVEL.eval("toUpperCase()",locale.localeCode).contains(MVEL.eval("toUpperCase()","es_ES"))')
+insert into BLC_PRICE_LIST_RULE(PRICE_LIST_RULE_ID,PRICE_LIST,PRIORITY,FRIENDLY_NAME) VALUES(3,2,3,'EUR Dollar if contains es_ES');
+INSERT INTO BLC_PL_RULE_MAP VALUES (3,3,'LOCALE');
+--if US then use EUR
+insert into BLC_PL_CRITERIA_RULE values(4,'MVEL.eval("toUpperCase()",locale.localeCode).contains(MVEL.eval("toUpperCase()","MX"))')
+insert into BLC_PRICE_LIST_RULE(PRICE_LIST_RULE_ID,PRICE_LIST,PRIORITY,FRIENDLY_NAME) VALUES(4,4,4,'Mexican Peso if contains MX');
+INSERT INTO BLC_PL_RULE_MAP VALUES (4,4,'LOCALE');
+--if GBP then use EUR
+insert into BLC_PL_CRITERIA_RULE values(5,'MVEL.eval("toUpperCase()",locale.localeCode).contains(MVEL.eval("toUpperCase()","FR"))')
+insert into BLC_PRICE_LIST_RULE(PRICE_LIST_RULE_ID,PRICE_LIST,PRIORITY,FRIENDLY_NAME) VALUES(5,2,5,'EUR Dollar if contains FR');
+INSERT INTO BLC_PL_RULE_MAP VALUES (5,5,'LOCALE');
+--if US then use EUR
+insert into BLC_PL_CRITERIA_RULE values(6,'MVEL.eval("toUpperCase()",locale.localeCode).contains(MVEL.eval("toUpperCase()","US"))')
+insert into BLC_PRICE_LIST_RULE(PRICE_LIST_RULE_ID,PRICE_LIST,PRIORITY,FRIENDLY_NAME) VALUES(6,1,6,'US Dollar if contains US');
+INSERT INTO BLC_PL_RULE_MAP VALUES (6,6,'LOCALE');
